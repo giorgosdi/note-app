efeddd25f30afc48d94542ed9fe1361ec0b9b7ce
Giorgos Dimitriou <giwrgosdi@gmail.com>
2017-11-05 14:56:21 +0000

    Add read functionality

--- app.js
+++ app.js
@@ -12,20 +12,29 @@ const argv = yargs.argv
 console.log('yargs', argv);
 
 var command = argv._[0]
-
 if (command == 'add') {
-  notes.addNote(argv.title, argv.body)
+  note = notes.addNote(argv.title, argv.body)
+
+  if (note != undefined) {
+    console.log("New note created with title", note.title);
+  } else {
+    console.log("Note already exists");
+  }
 } else if (command == "list") {
   notes.getAll()
 } else if(command == "read") {
-  notes.getNote(argv.title)
+  note = notes.getNote(argv.title)
+  if(note != undefined) {
+    console.log("I got a note");
+    debugger
+    console.log("Note title : ", note.title);
+    console.log("Note Body : ", note.body);
+    debugger
+  } else {
+    console.log("Note not found in file");
+  }
 } else if(command == "remove") {
   notes.removeNote(argv.title)
 } else {
   console.log("command not recognised");
 }
-
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.age}`);
--- notes.js
+++ notes.js
@@ -1,29 +1,35 @@
 console.log("Starting notes.js");
 const fs = require('fs')
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync("notes-data.json")
+    notes = JSON.parse(notesString);
+    return notes
+  } catch (err) {
+    return [];
+  }
+}
+
+var saveNotes = (notes) => {
+  fs.writeFileSync("notes-data.json", JSON.stringify(notes))
+}
+
 var addNote = (title, body) => {
-  var notes = []
+  var notes = fetchNotes()
   var note = {
     title,
     body
   }
 
-  try {
-    var notesString = fs.readFileSync("notes-data.json")
-    notes = JSON.parse(notesString);
-  } catch (err) {
-
-  }
 
   var duplicateNotes = notes.filter((note) => note.title === title)
 
   if (duplicateNotes.length === 0) {
     notes.push(note)
-    fs.writeFileSync("notes-data.json", JSON.stringify(notes))
-  } else {
-    console.log("Duplicate title found");
+    saveNotes(notes)
+    return note
   }
-
 }
 
 var getAll = () => {
@@ -31,11 +37,24 @@ var getAll = () => {
 }
 
 var getNote = (title) => {
-  console.log('Getting the note with title', title);
+  var notes = fetchNotes()
+  if(notes.length > 0) {
+    var read_note = notes.filter((note) => note.title === title)
+    return read_note[0];
+  }
 }
 
 var removeNote = (title) => {
-  console.log('Removing the note with title', title);
+  var notes = fetchNotes()
+  if(notes.length > 0) {
+    new_notes = notes.filter((note) => note.title !== title)
+    saveNotes(new_notes)
+    if(notes.length > new_notes.length) {
+      console.log(`The note with title ${title} was removed succesfully`);
+    } else {
+      console.log(`Note with title \"${title}\" does not exists`);
+    }
+  }
 }
 
 module.exports = {
